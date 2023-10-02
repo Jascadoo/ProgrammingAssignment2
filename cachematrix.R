b8123fcabf0aaf6d631d21c6057e287add76c11e
Jascadoo <145214504+Jascadoo@users.noreply.github.com>
2023-10-02 19:14:49 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ini <- NULL
+  set <- function(y){
+    x<<-y
+    ini <<- NULL
+  }
+  get <- function(){
+    x
+  }
+  setinv <- function(inversed) {
+    ini <<- inversed
+  }
+  getinv <- function() ini
+  list(set = set, get = get, setinv = setinv, getinv = getinv)  
 }
 
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ini <- x$getinv()
+  if(!is.null(ini)){
+    message("getting cached data")
+    return(ini)
+  }
+  ninv <- x$get()
+  ini <- solve(ninv,...)
+  x$setinv(ini)
+  ini
 }
