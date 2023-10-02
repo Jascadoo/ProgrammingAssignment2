1cb0253b6dab3bbbdaf2b7adc6bfdcae63f57300
Jascadoo <145214504+Jascadoo@users.noreply.github.com>
2023-10-02 21:14:40 +0800

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,26 +2,29 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-  ini <- NULL
-  set <- function(y){
+## create a matrix 
+makeCacheMatrix <- function(x = matrix()) {     
+  ini <- NULL        
+  ##set the value of matrix
+  set <- function(y){    
     x<<-y
     ini <<- NULL
   }
+  ## Get the matrix
   get <- function(){
     x
   }
-  setinv <- function(inversed) {
+   ## Set Inverse of the matrix
+  setinv <- function(inversed) {   
     ini <<- inversed
   }
+  ## Get Inverse of the matrix
   getinv <- function() ini
   list(set = set, get = get, setinv = setinv, getinv = getinv)  
 }
 
-
 ## Write a short comment describing this function
-
+## This function gets the matrix value from the makeCacheMartix object and returns the inverted matrix to the object again 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   ini <- x$getinv()
@@ -31,6 +34,7 @@ cacheSolve <- function(x, ...) {
   }
   ninv <- x$get()
   ini <- solve(ninv,...)
+  ## Set the inversed matrix to object makeCacheMartix
   x$setinv(ini)
   ini
 }
